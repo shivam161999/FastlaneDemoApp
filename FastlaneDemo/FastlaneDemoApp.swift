bcae4a5ccef7f46d58b8ad3c4cac85d214b1f9df
Thinkitive <thinkitive@Thinkitives-MacBook-Pro.local>
2023-06-27 01:23:55 +0530

    initial commit

---
+++ FastlaneDemo/FastlaneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlaneDemoApp.swift
+//  FastlaneDemo
+//
+//  Created by Thinkitive on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FastlaneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
